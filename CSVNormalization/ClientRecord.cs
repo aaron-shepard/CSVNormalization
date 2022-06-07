3634a2721af597ba6d47362fe6850a3437a39ed4
Aaron <aaron.c.shepard@gmail.com>
2022-06-07 15:11:47 -0700

    add error checking, do not write record to ouput if normalization fails

--- CSVNormalization/ClientRecord.cs
+++ CSVNormalization/ClientRecord.cs
@@ -9,8 +9,8 @@ namespace CSVNormalization
 {
     internal class ClientRecord
     {
-        private TimeZoneInfo estZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
-        private TimeZoneInfo pstZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+        private readonly TimeZoneInfo estZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+        private readonly TimeZoneInfo pstZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
 
         public string Timestamp { get; set; }
         public string Address { get; set; }
@@ -21,36 +21,46 @@ namespace CSVNormalization
         public string TotalDuration { get; set; }
         public string? Notes { get; set; }
 
-        public void NormalizeData()
+        public bool NormalizeData()
         {
-            // TODO - change to return if failure
-            this.ConvertTimestampToRFC3339();
+            // return false if any normalization functions fail
+            bool success = true;
+
+            success = success && this.ConvertTimestampToRFC3339();
             this.NormalizeZip();
             this.NormalizeFullName();
-            this.FooDuration = this.NormalizeDuration(this.FooDuration);
-            this.BarDuration = this.NormalizeDuration(this.BarDuration);
-            this.NormalizeTotalDuration();
+            this.FooDuration = NormalizeDuration(this.FooDuration);
+            success = success && (this.FooDuration != null);
+            this.BarDuration = NormalizeDuration(this.BarDuration);
+            success = success && this.NormalizeTotalDuration();
+
+            return success;
         }
 
-        private void ConvertTimestampToRFC3339()
+        private bool ConvertTimestampToRFC3339()
         {
             // assume comes in PST
-            // TODO -  ISO 8601
+            // TODO -  ISO 8601 compliant as well?
+            // this check should be in BL lib, prefereably pass the timezones as args
 
             DateTime dt;
             if (!DateTime.TryParse(this.Timestamp, out dt))
             {
-                // TODO - handle parse failure
+                Console.Error.WriteLine("Invalid Timestamp: " + this.Timestamp);
+                return false;
             }
 
             dt = TimeZoneInfo.ConvertTime(dt, pstZone);
             dt = TimeZoneInfo.ConvertTime(dt, estZone);
 
             this.Timestamp = dt.ToString("yyyy-MM-dd HH:mm:ss.fffzzz");
+            return true;
         }
 
         private void NormalizeZip()
         {
+            // TODO - confirm if expect any alpha zip codes, otherwise validate numbers only
+            // this check should be in BL lib
             while (this.ZIP.Length < 5)
             {
                 this.ZIP = "0" + this.ZIP;
@@ -62,30 +72,41 @@ namespace CSVNormalization
             this.FullName = this.FullName.ToUpper();
         }
 
-        private string NormalizeDuration(string duration)
+        private static string? NormalizeDuration(string duration)
         {
-            string[] parts = duration.Split(':');
-            int hours = int.Parse(parts[0]);
-            int minutes = int.Parse(parts[1]);
-            // seconds includes MS as decimal, need to extract
-            double seconds = double.Parse(parts[2]);
-            int milliseconds = (int)(((decimal)seconds % 1) * 1000);
+            try
+            {
+                // TODO this check should be in BL lib
+                string[] parts = duration.Split(':');
+                int hours = int.Parse(parts[0]);
+                int minutes = int.Parse(parts[1]);
+                // seconds includes MS as decimal, need to extract
+                double seconds = double.Parse(parts[2]);
+                int milliseconds = (int)(((decimal)seconds % 1) * 1000);
 
-            TimeSpan ts = new TimeSpan(0, hours, minutes, (int)seconds, milliseconds);
-
-            return ts.TotalSeconds.ToString();
+                TimeSpan ts = new TimeSpan(0, hours, minutes, (int)seconds, milliseconds);
+                return ts.TotalSeconds.ToString();
+            } catch(Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Console.Error.WriteLine("Invalid duration: " + duration);
+                return null;
+            }
+            
         }
 
-        private void NormalizeTotalDuration()
+        private bool NormalizeTotalDuration()
         {
+            // TODO this check should be in BL lib
             double fooVal, barVal;
             this.TotalDuration = "0";
             if (Double.TryParse(this.FooDuration, out fooVal) && Double.TryParse(this.BarDuration, out barVal))
             {
                 double totalVal = fooVal + barVal;
                 this.TotalDuration = String.Format("{0:0.###}", totalVal);
+                return true;
             }
-            // TODO - cannot add values
+            return false;
         }
     }
 }
--- CSVNormalization/Program.cs
+++ CSVNormalization/Program.cs
@@ -5,26 +5,28 @@ using CSVNormalization;
 using System.Globalization;
 using System.Text;
 
-var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+var csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
 {
     Encoding = Encoding.UTF8,
     Delimiter = ","
 };
 
+// TODO - validate CSV file is inputted
+// add error/warning to console to indicate bad CSV
 using (StreamReader textReader = new(Console.OpenStandardInput(), Encoding.UTF8))
-using (CsvReader csv = new(textReader, configuration))
+using (CsvReader csv = new(textReader, csvConfiguration))
 {
     var clientRecordList = csv.GetRecords<ClientRecord>();
 
-    using (TextWriter writer = new StreamWriter(Console.OpenStandardOutput()))
-    {
-        CsvWriter csvWriter = new(writer, configuration);
+    using TextWriter writer = new StreamWriter(Console.OpenStandardOutput());
+    CsvWriter csvWriter = new(writer, csvConfiguration);
 
-        csvWriter.WriteHeader<ClientRecord>();
-        csvWriter.NextRecord();
-        foreach (var clientRecord in clientRecordList)
+    csvWriter.WriteHeader<ClientRecord>();
+    csvWriter.NextRecord();
+    foreach (var clientRecord in clientRecordList)
+    {
+        if (clientRecord.NormalizeData())
         {
-            clientRecord.NormalizeData();
             csvWriter.WriteRecord<ClientRecord>(clientRecord);
             csvWriter.NextRecord();
         }
